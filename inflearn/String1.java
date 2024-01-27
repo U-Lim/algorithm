493f0c0fbc97c31d2f251af400776f2e3f575b3f
U-Lim <ylkk329@gmail.com>
2024-01-28 01:42:04 +0900

    인프런_String_1.문자찾기

---
+++ inflearn/String1.java
@@ -0,0 +1,29 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class String1 {
+	
+	private int solution(String str, char c) {
+		int result = 0;
+		
+		str = str.toLowerCase();
+		c = Character.toLowerCase(c); // char형 변수의 소문자 변환
+		
+		for(char tmp : str.toCharArray()) { // 향상된 for문, 배열 또는 컬렉션만 가능
+			if(tmp == c) result++;
+		}
+		
+		return result;
+	}
+	 
+	public static void main(String[] args) {
+		String1 s = new String1();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char c = sc.next().charAt(0);
+		
+		System.out.println(s.solution(str, c));
+	}
+
+}
